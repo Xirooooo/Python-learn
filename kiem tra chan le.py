9063640f0e5464f7c20cdb73a4bd5a1b4a1b66eb
Xirooooo <103835936+Xirooooo@users.noreply.github.com>
2022-05-16 21:37:04 +0700

    Add files via upload

---
+++ kiem tra chan le.py
@@ -0,0 +1,10 @@
+a = input()
+while int(a) ==0:
+    a = input("nhap")
+if int(a) % 2 == 0:
+    print('số chẵn')
+elif int(a) % 2 == 1:
+    print('số lẻ')
+else:
+    print('không phải số tự nhiên')
+ 
